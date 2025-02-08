c9aa5d2942a6ca352851ea97cb62c27574af6447
Ane Marinheiro Weber <anemariweber@gmail.com>
2025-02-08 10:43:27 +0100

    middleware for authorization

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -2,11 +2,16 @@ import { Router } from "express"
 import { ProductsController } from "@/controllers/products-controller"
 
 import {ensureAuthenticated} from "@/middlewares/ensureAuthenticated"
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization"
 
 const productsRoutes = Router()
 const productsController = new ProductsController()
 
+//apply authorization in all routes bellow:
+//productsRoutes.use(verifyUserAuthorization(["sale", "admin"]))
+
+//or use the authorization in a specific route:
 productsRoutes.get("/", productsController.index)
-productsRoutes.post("/", ensureAuthenticated, productsController.create)
+productsRoutes.post("/", ensureAuthenticated, verifyUserAuthorization(["sale", "admin"]), productsController.create)
 
 export { productsRoutes }
